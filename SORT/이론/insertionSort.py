9531c016de93e6a552200efabb5aaaf6d106d9a4
khlee <panpan68@naver.com>
2023-12-19 18:00:35 +0900

    삽입정렬 이론

---
+++ SORT/이론/insertionSort.py
@@ -0,0 +1,15 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    print("I: ", end='')
+    print(i) #첫번째 인덱스부터 시작한다!
+    for j in range(i, 0, -1): #인덱스 i부터 1까지 1씩 감소하며 반복하는 문법
+        print("J: ", end='')
+        print(array[j])
+        print(array[j - 1])
+        if array[j] < array[j - 1]: #자신과 자기 이전의 데이터와 비교한다.
+            array[j], array[j - 1] = array[j - 1], array[j] #이전의 데이터가 더 작으면 내가 그 데이터의 왼쪽으로 간다.
+        else: #자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+            break;
+
+print(array)
--- SORT/이론/selectionSort.py
+++ SORT/이론/selectionSort.py
@@ -4,7 +4,7 @@ for i in range(len(array)):
     min_index = i # 선택된 제일 앞의 원소.
     for j in range(i+1, len(array)):
         if array[min_index] > array[j]:
-            min_index = j
+            min_index = j #선택되었던 원소보다 작은 인덱스 값을 발견하면 값을 바꿔준다.
     array[i], array[min_index] = array[min_index], array[i] #스와프
 
 print(array)
